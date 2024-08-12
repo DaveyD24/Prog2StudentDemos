692f0c1266856e393679dcc53a495ab220fbc3be
Davey <dtdyer123@gmail.com>
2024-08-13 09:27:11 +1000

    Initial commit

---
+++ Week 2/Program.java
@@ -0,0 +1,20 @@
+public class Program {
+    public static void main(String[] args) {
+        int[] marks = {87, 76, 75, 85, 60, 51, 50, 50, 89, 98, 94, 82, 80, 91, 67};
+
+        //Compute sum
+
+        //Compute average
+
+        //Print results
+        
+    }
+
+    private static boolean isAllPassing(int[] marks) {
+        return false;
+    }
+
+    private static boolean anyHighAcheivers(int[] marks) {
+        return false;
+    }
+}
---
+++ Week 2/program.py
@@ -0,0 +1,14 @@
+
+def isAllPassing(marks: list) -> bool:
+    pass
+
+def anyHighAcheivers(marks: list) -> bool:
+    pass
+
+marks = [87, 76, 75, 85, 60, 51, 50, 50, 89, 98, 94, 82, 80, 91, 67]
+
+#Compute sum
+
+#Compute Average
+
+#Print Results

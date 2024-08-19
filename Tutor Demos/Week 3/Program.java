87479da0eb8db660c5543d2f3ec7161a74bb58e7
DaveyD24 <69884791+DaveyD24@users.noreply.github.com>
2024-08-20 00:19:47 +1000

    Week 3 Tutor Demo Scaffold

---
+++ Tutor Demos/Week 3/Program.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class Program {
+
+    LinkedList<String> names = new LinkedList<String>();
+
+    private void use() {
+        //Read in names until the user enters X
+
+        //Print whether any emails were spotted
+    }
+
+    public static void main(String[] args) {
+        new Program().use();
+    }
+}

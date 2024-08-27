ed2d061a4941462e2a9071801234244c2edb282d
Davey <dtdyer123@gmail.com>
2024-08-27 13:22:13 +1000

    Week 4 Tutor Demo Solution

--- Tutor Demos/Week 4/Sentence.java
+++ Tutor Demos/Week 4/Sentence.java
@@ -6,13 +6,57 @@ public class Sentence {
         this.sentence = sentence;
     }
 
+    public int wordCount() {
+        return sentence.split(" ").length;
+    }
+
+    public boolean isPascalCase() {
+        for (String word : sentence.split(" ")) {
+            if (Character.isLowerCase(word.charAt(0))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void display() {
+        System.out.println(sentence + ":");
+        System.out.println("Total words in sentence: " + wordCount());
+        showZCount();
+        System.out.println(isPascalCase() ? "Sentence Is In Pascal Case" : "sentence is not in pascal case");
+        System.out.println("--------------------------------");
+    }
+
+    public void showZCount() {
+        System.out.println("Number of z words in this sentence: " + zCount());
+    }
+
+    private int zCount() {
+        int count = 0;
+        for (String word : sentence.split(" ")) {
+            if (containsZ(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean containsZ(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.toLowerCase().charAt(i) == 'z') {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     public static void main(String[] args) {
-        String[] sentences = {"David is zhe worst tutor ever", "The Big Brown Fox Did Some Stuff I Guess", "hi bruzz yez sir"};
+        String[] sentences = {"David is zhe worst tutor ever", "The Big Brown Fox Did Some Stuff I Guess", "time to muzz", "Example sentence", "exampleZ SentenzeZ"};
         for (String sentence : sentences) {
-            // Sentence s = new Sentence(sentence);
-            // s.showZCount();
-            // System.out.println(s.isPascalCase() ? "Very Nice" : "not good");
+            new Sentence(sentence).display();
         }
     }
 }
+
+//github.com/DaveyD24/Prog2StudentDemos

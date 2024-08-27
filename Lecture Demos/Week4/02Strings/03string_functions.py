ac66e36a31487b11b903e7c7cc9f119d9857b8ff
Davey <dtdyer123@gmail.com>
2024-08-27 10:23:25 +1000

    Revert "Lecture 4 Content"
    This reverts commit 68fffc8d48ca2edcca7dd8e1936dbd7a72d3729b.

--- Lecture Demos/Week4/02Strings/03string_functions.py
+++
@@ -1,9 +0,0 @@
-david = "David"
-
-print(f"Position of v:          {david.index('v')}")
-print(f"Starts with D?:         {david.startswith('D')}")
-print(f"Ends with CAPITAL D?:   {david.endswith('D')}")
-print(f"Are these the same?:    {david == 'Jenny'}")
-print(f"Does david has his id?: {'id' in david}")
-print(f"Big David:              {david.upper()}")
-print(f"Small David:            {david.lower()}")

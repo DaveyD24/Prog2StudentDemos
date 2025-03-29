bdc8541381a53ec3e272410a8aca4e290399cd89
Davey Dyer <69884791+DaveyD24@users.noreply.github.com>
2025-03-29 16:25:02 +1100

    Week 4 Java & Python Changes
    Java comment added
    Python demo created

--- Tutor Demos/Week 4/Sentence.java
+++ Tutor Demos/Week 4/Sentence.java
@@ -1,30 +1,22 @@
 public class Sentence {
 
-    private String sentence;
+    private final String sentence;
 
     public Sentence(String sentence) {
         this.sentence = sentence;
     }
 
-    public int wordCount() {
-        return sentence.split(" ").length;
-    }
-
-    public boolean isPascalCase() {
-        for (String word : sentence.split(" ")) {
-            if (Character.isLowerCase(word.charAt(0))) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public void display() {
+        System.out.println("--------------------------------");
         System.out.println(sentence + ":");
         System.out.println("Total words in sentence: " + wordCount());
         System.out.println("Number of z words in this sentence: " + zCount());
         System.out.println(isPascalCase() ? "Sentence Is In Pascal Case" : "sentence is not in pascal case");
         System.out.println("--------------------------------");
+    }
+
+    public int wordCount() {
+        return sentence.split(" ").length;
     }
 
     private int zCount() {
@@ -46,6 +38,15 @@ public class Sentence {
         return false;
     }
 
+    //Advanced feature
+    public boolean isPascalCase() {
+        for (String word : sentence.split(" ")) {
+            if (Character.isLowerCase(word.charAt(0))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         String[] sentences = {"David is zhe worst tutor ever", "The Big Brown Fox Did Some Stuff I Guess", "time to muzz", "Example sentence", "exampleZ SentenzeZ"};
@@ -54,5 +55,3 @@ public class Sentence {
         }
     }
 }
-
-//github.com/DaveyD24/Prog2StudentDemos

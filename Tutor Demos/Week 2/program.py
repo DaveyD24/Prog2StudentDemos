44a426d17cc2852724056449a947584468fd5bb9
Davey Dyer <69884791+DaveyD24@users.noreply.github.com>
2025-03-29 16:02:29 +1100

    Week 2 Java & Python Changes
    Java Demo uncommented
    Python Demo altered to use formatted string and class structure

--- Tutor Demos/Week 2/program.py
+++ Tutor Demos/Week 2/program.py
@@ -1,28 +1,31 @@
-#Every pattern
-def isAllPassing(marks: list) -> bool:
-    for mark in marks:
-        if mark < 50:
-            return False
-    return True
+class Program:
 
-#Any pattern
-def anyHighAcheivers(marks: list) -> bool:
-    for mark in marks:
-        if mark == 100:
-            return True
-    return False
+    #Every pattern
+    def isAllPassing(marks: list) -> bool:
+        for mark in marks:
+            if mark < 50:
+                return False
+        return True
 
-marks = [87, 76, 75, 85, 60, 51, 49, 50, 89, 100, 94, 82, 80, 91, 67]
+    #Any pattern
+    def anyHighAchievers(marks: list) -> bool:
+        for mark in marks:
+            if mark == 100:
+                return True
+        return False
 
-#Compute sum
-sum = 0
-for mark in marks:
-    sum = sum + mark
+    if __name__ == "__main__":
+        marks = [87, 76, 75, 85, 60, 51, 49, 50, 89, 100, 94, 82, 80, 91, 67]
 
-#Compute Average
-average = sum / len(marks)
+        #Compute sum
+        sum = 0
+        for mark in marks:
+            sum += mark
 
-#Print Results
-print("Average Mark: " + str(average))
-print("100% Pass Rate" if isAllPassing(marks) else "Below 100% Pass Rate")
-print("Someone Achieved 100" if anyHighAcheivers(marks) else "No High Achievers")
+        #Compute Average
+        average = sum / len(marks)
+
+        #Print Results
+        print(f"Average Mark: {average:.2f}")
+        print("100% Pass Rate" if isAllPassing(marks) else "Below 100% Pass Rate")
+        print("Someone Achieved 100" if anyHighAchievers(marks) else "No High Achievers")
